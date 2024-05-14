2e7f908e9326cbc3e8dc51cf7ac48a70af7a58f1
Hau Tran <hautd@crownx.masangroup.com>
2024-05-14 17:47:05 +0700

    Start base flutter google form

--- lib/create_form/create_form_view.dart
+++ lib/create_form/create_form_view.dart
@@ -8,13 +8,23 @@ class CreateFormView extends StatefulWidget {
 }
 
 class _CreateFormViewState extends State<CreateFormView> {
+  // controller
   final titleController = TextEditingController();
   final titleDescriptionController = TextEditingController();
 
+  //index bottom bar
+  int selectIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      selectIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.purple[50],
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
@@ -25,6 +35,41 @@ class _CreateFormViewState extends State<CreateFormView> {
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: false,
+        selectedFontSize: 12,
+        unselectedFontSize: 12,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline_sharp),
+            label: ''
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.send_to_mobile_sharp),
+              label: ''
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.text_fields),
+              label: ''
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.image_outlined),
+              label: ''
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.ondemand_video_outlined),
+              label: ''
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.list_outlined),
+              label: ''
+          ),
+        ],
+        currentIndex: selectIndex,
+        selectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
     );
   }
 
@@ -34,14 +79,20 @@ class _CreateFormViewState extends State<CreateFormView> {
       height: 120,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8),
-        border: const Border(top: BorderSide(width: 5, color: Colors.purpleAccent))
+        color: Colors.white,
+        border: const Border(top: BorderSide(width: 5, color: Colors.purple))
       ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
         children: [
           Container(
             width: MediaQuery.of(context).size.width,
-            height: 80,
+            child: Text('Untitled Form', style: TextStyle(fontWeight: FontWeight.w400, fontSize: 25),),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: Text('Form Description', style: TextStyle(fontWeight: FontWeight.w400, fontSize: 16, color: Colors.grey),),
           )
         ],
       ),
